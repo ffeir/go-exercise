722bbe6cbffb9fd2c0ea96114645a80160cf8c04
Jianfei Shu <jianfei.shu@shopee.com>
2018-11-24 14:42:16 +0800

    add channels exercise

---
+++ channels/calc_refactor.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func digits(number int, dchnl chan int) {
+	for number != 0 {
+		digit := number % 10
+		dchnl <- digit
+		number /= 10
+	}
+	close(dchnl)
+}
+func calcSquares2(number int, squareop chan int) {
+	sum := 0
+	dch := make(chan int)
+	go digits(number, dch)
+	for digit := range dch {
+		sum += digit * digit
+	}
+	squareop <- sum
+}
+
+func calcCubes2(number int, cubeop chan int) {
+	sum := 0
+	dch := make(chan int)
+	go digits(number, dch)
+	for digit := range dch {
+		sum += digit * digit * digit
+	}
+	cubeop <- sum
+}
+
+func main() {
+	number := 589
+	sqrch := make(chan int)
+	cubech := make(chan int)
+	go calcSquares2(number, sqrch)
+	go calcCubes2(number, cubech)
+	squares, cubes := <-sqrch, <-cubech
+	fmt.Println("Final output", squares+cubes)
+}
